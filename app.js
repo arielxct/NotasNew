60e3de1c65695600e2db865a64e58b2b231cfc3d
Ariel CT <campostula@gmail.com>
2023-05-12 16:15:58 -0300

    hice uploads de los archivos 12may23

---
+++ app.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(3000, () => 
+    console.log("Servidor Corriendo en Proyecto Block de Notas")
+);
